3ceed03ffb95611e0d87ec571cc9e49bc7c0c0ff
Mayan Kumar Mishra <91338576+mishramayan@users.noreply.github.com>
2026-02-25 10:50:12 -0600

    Update nav.jsx

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -12,7 +12,7 @@ const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active': ''}><ImHome/></a>
+      <a href="#home" onClick={() => setActiveNav('#home')} className={activeNav === '#home' ? 'active': ''}><ImHome/></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active': ''}><FaUserAlt/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active': ''}><BiBook/></a>
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active': ''}><GoProject/></a>
